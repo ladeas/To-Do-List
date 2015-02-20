93cfcb8d57117cbd1d148b89237aae0cd0b49211
Stavros Ladeas <stavro@stavro3000.local>
2015-02-20 11:37:23 -0500

    Initial Commit

---
+++ Real To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Real To Do List
+//
+//  Created by Stavros Ladeas on 2/20/15.
+//  Copyright (c) 2015 Stavros Ladeas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
